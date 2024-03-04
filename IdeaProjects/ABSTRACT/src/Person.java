05696f6eed7fbdf18d32016699ac6bfa58f4a8e0
HUY1602 <mqhuy1602@gmail.com>
2024-03-04 21:45:58 +0700

    co lam duoc viec k

---
+++ IdeaProjects/ABSTRACT/src/Person.java
@@ -0,0 +1,10 @@
+public class Person {
+    private String name;
+    private String address;
+    private List<Vehicle> vehicleList;
+    public Person(String name, String address) {
+        this.name= name;
+        this.address = address;
+    }
+    public void 
+}
